06f92efa9a2e8991c4ec5ec25551b1d207db8e2a
canusorn <39732595+canusorn@users.noreply.github.com>
2024-05-10 19:40:16 +0700

    show all chart

---
+++ src/components/OverviewChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="_loaded" class="container">
+    <Line  :data="chartData" />
+  </div>
+    <h5 v-else >Loading...</h5>
+</template>
+
+<script>
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS, CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+ChartJS.register(CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend)
+
+export default {
+  name: 'OverviewChart',
+  props: ["chartDataVariable", "chartDataLabel"],
+  components: { Line },
+  data() {
+    return {
+      _loaded: false,
+      chartData: null
+    }
+  },
+  mounted() {
+
+    this._loaded = false
+
+    var dataout = { labels: [], datasets: [] }
+
+    console.log(this.chartDataVariable);
+
+    dataout.labels = this.chartDataLabel;
+
+    dataout.datasets = [this.chartDataVariable];
+
+    this.chartData = dataout;
+
+    this._loaded = true
+
+  }
+}
+</script>
