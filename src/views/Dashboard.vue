f1dfed1bdd3b64db344e238fd68193e111c00589
canusorn <39732595+canusorn@users.noreply.github.com>
2024-05-29 17:46:52 +0700

    mqtt client connect

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -14,6 +14,7 @@
 <script>
 import OverviewChart from '@/components/OverviewChart.vue';
 import store from "@/store/index.js";
+import mqtt from "mqtt";
 
 import {
     Chart as ChartJS, CategoryScale,
@@ -40,62 +41,182 @@ export default {
             espid: 7884150,
             loaded: false,
             chartData: null,
-            varAmount: 0
+            varAmount: 0,
+            connection: {
+                protocol: "ws",
+                host: "192.168.0.101",
+                // ws: 8083; wss: 8084
+                port: 8888,
+                // for more options, please refer to https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
+                clean: true,
+                connectTimeout: 30 * 1000, // ms
+                reconnectPeriod: 4000, // ms
+                clientId: "emqx_vue_" + Math.random().toString(16).substring(2, 8),
+                // auth
+                username: "anusorn1998@gmail.com",
+                password: "emqx_test",
+            },
+            subscription: {
+                topic: "/#",
+                qos: 0,
+            },
+            publish: {
+                topic: "topic/browser",
+                qos: 0,
+                payload: '{ "msg": "Hello, I am browser." }',
+            },
+            receiveNews: "",
+            qosList: [0, 1, 2],
+            client: {
+                connected: false,
+            },
+            subscribeSuccess: false,
+            connecting: false,
+            retryTimes: 0,
         }
     },
-    async mounted() {
+    methods: {
+        initChart() {
+            this.loaded = false
 
-        this.loaded = false
+            fetch(store.getters.serverURL + "/api/timedata/" + this.espid + "/getlastdata",
+                {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${store.getters.accessToken}`
+                    }
+                }
+            )
+                .then((res) => res.json())
+                .then((data) => {
+                    // console.log(typeof data)
+                    // console.log(data)
 
-        fetch(store.getters.serverURL + "/api/timedata/" + this.espid + "/getlastdata",
-            {
-                method: "GET",
-                headers: {
-                    Authorization: `Bearer ${store.getters.accessToken}`
+                    this.loaded = false;
+
+                    let size = Object.keys(data[0]).length;
+                    this.varAmount = size - 2;
+                    console.log("object size : " + size);
+
+                    var dataout = { labels: [], datasets: [] }
+
+                    let labels = [], datasets = [];
+
+                    data.forEach((document) => {
+
+                        for (let i = 1; i <= size - 2; i++) {
+                            if (!datasets[i - 1]) {
+                                datasets.push({ label: Object.keys(data[0])[i], data: [] })
+                            }
+                            datasets[i - 1].data.push(Object.values(document)[i])
+                        }
+                        labels.push(Object.values(document)[size - 1])
+
+                    });
+
+
+                    dataout.labels = labels;
+                    dataout.datasets = datasets;
+
+
+                    this.chartData = dataout;
+                    // console.log(this.chartData);
+                    this.loaded = true
+                })
+                .catch((err) => {
+                    console.log("error occured", err)
+                });
+        },
+        initData() {
+            this.client = {
+                connected: false,
+            };
+            this.retryTimes = 0;
+            this.connecting = false;
+            this.subscribeSuccess = false;
+        },
+        handleOnReConnect() {
+            this.retryTimes += 1;
+            if (this.retryTimes > 5) {
+                try {
+                    this.client.end();
+                    this.initData();
+                    this.$message.error("Connection maxReconnectTimes limit, stop retry");
+                } catch (error) {
+                    this.$message.error(error.toString());
                 }
             }
-        )
-            .then((res) => res.json())
-            .then((data) => {
-                // console.log(typeof data)
-                // console.log(data)
-
-                this.loaded = false;
-
-                let size = Object.keys(data[0]).length;
-                this.varAmount = size - 2;
-                console.log("object size : " + size);
-
-                var dataout = { labels: [], datasets: [] }
-
-                let labels = [], datasets = [];
-
-                data.forEach((document) => {
-
-                    for (let i = 1; i <= size - 2; i++) {
-                        if (!datasets[i - 1]) {
-                            datasets.push({ label: Object.keys(data[0])[i], data: [] })
-                        }
-                        datasets[i - 1].data.push(Object.values(document)[i])
-                    }
-                    labels.push(Object.values(document)[size - 1])
-
-                });
-
-
-                dataout.labels = labels;
-                dataout.datasets = datasets;
-
-
-                this.chartData = dataout;
-                // console.log(this.chartData);
-                this.loaded = true
+        },
+        createConnection() {
+            try {
+                this.connecting = true;
+                const { protocol, host, port, ...options } = this.connection;
+                const connectUrl = `${protocol}://${host}:${port}`;
+                this.client = mqtt.connect(connectUrl, options);
+                if (this.client.on) {
+                    this.client.on("connect", () => {
+                        this.connecting = false;
+                        console.log("Connection succeeded!");
+                    });
+                    this.client.on("reconnect", this.handleOnReConnect);
+                    this.client.on("error", (error) => {
+                        console.log("Connection failed", error);
+                    });
+                    this.client.on("message", (topic, message) => {
+                        this.receiveNews = this.receiveNews.concat(message);
+                        console.log(`Received message ${message} from topic ${topic}`);
+                    });
+                }
+            } catch (error) {
+                this.connecting = false;
+                console.log("mqtt.connect error", error);
+            }
+        },
+        doSubscribe() {
+            const { topic, qos } = this.subscription
+            this.client.subscribe(topic, { qos }, (error, res) => {
+                if (error) {
+                    console.log('Subscribe to topics error', error)
+                    return
+                }
+                this.subscribeSuccess = true
+                console.log('Subscribe to topics res', res)
             })
-            .catch((err) => {
-                console.log("error occured", err)
-            });
+        }, doUnSubscribe() {
+            const { topic } = this.subscription
+            this.client.unsubscribe(topic, error => {
+                if (error) {
+                    console.log('Unsubscribe error', error)
+                }
+            })
+        },
+        doPublish() {
+            const { topic, qos, payload } = this.publish
+            this.client.publish(topic, payload, { qos }, error => {
+                if (error) {
+                    console.log('Publish error', error)
+                }
+            })
+        },
+        destroyConnection() {
+            if (this.client.connected) {
+                try {
+                    this.client.end(false, () => {
+                        this.initData()
+                        console.log('Successfully disconnected!')
+                    })
+                } catch (error) {
+                    console.log('Disconnect failed', error.toString())
+                }
+            }
+        }
 
-
+    },
+    async mounted() {
+        this.initChart();
+        this.initData();
+        this.createConnection();
+        this.doSubscribe();
     }
 }
 </script>
